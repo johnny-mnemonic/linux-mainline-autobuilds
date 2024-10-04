cc9ffdda2e9128bd4bdd810b791fde0be8ba23af
Johnny Mnemonic <jm@machine-hall.org>
2024-10-04 20:25:42 +0200

    Include configuration, tools and patches needed for build

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# linux-mainline-autobuilds
-Linux mainline builds for IA-64
+# Linux mainline autobuilds for ia64#
+
+Builds happen each day in the morning and use the latest ia64 patch set for Linux mainline.
+
+If the build worked, the resulting build artifacts (incl.:
+
+* the build log
+* the used kernel config
+* the symbol table
+* the compressed/uncompressed kernel images
+* the created kernel modules for the used kernel config
+
+...are available for download.
---
+++ tools/run-ski-test.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export SKI_USE_FAKE_XTERM=1
+
+_skiBootLoader="$1"
+_hpSimLinuxKernel="$2"
+_skiPathVar="/bin:/sbin:/usr/bin:/usr/sbin"
+
+bski \
+	${_skiBootLoader} \
+	${_hpSimLinuxKernel} \
+	root=/dev/sda \
+	simscsi=./sd \
+	simeth=enp3s0f0 \
+	init=/root/bin/ski_test.bash \
+	PATH=${_skiPathVar} \
+	rw \
+	memblock=debug &
+
+_skiPID=$!
+
+sleep 0.25
+
+_skiLog=ski.*
+
+tail -f -n +1 ${_skiLog} &
+
+_tailPID=$!
+
+while ! grep 'INFO: Ski execution finished.' ${_skiLog} &>/dev/null; do
+
+	sleep 10
+done
+
+sleep 5
+
+kill ${_tailPID}
+kill ${_skiPID}
+
+if grep 'INFO: Ski test succeeded.' ${_skiLog} &>/dev/null; then
+
+	rm ${_skiLog}
+	exit 0
+else
+	rm ${_skiLog}
+	exit 1
+fi
